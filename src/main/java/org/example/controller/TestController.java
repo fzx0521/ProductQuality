028dec6e7db6eb9872bd11fe1974910924de2a8b
fzx0521 <1703181617@qq.com>
2022-11-11 10:39:15 +0800

    TestController第一次

--- src/main/java/org/example/controller/TestController.java
+++ src/main/java/org/example/controller/TestController.java
@@ -2,6 +2,6 @@ package org.example.controller;
 
 public class TestController {
     public static void main(String[] args) {
-        System.out.println("hhh");
+        System.out.println("哈哈哈");
     }
 }
